f0d55bad5701a999d09545960af48a257191c090
John Barry <john.olivierb@gmail.com>
2023-09-14 23:18:56 -0400

    Switch for TOURNOIS ARAM

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <NavBar />
       <div className="flex flex-col items-center justify-center w-full pt-48 pb-56 text-center z-50">
         <h1 className="text-primary font-bold w-full text-9xl">LEAGUE OF LEGENDS</h1>
-        <h2 className="text-white font-bold w-full text-9xl">TOURNOIS HAQ</h2>
+        <h2 className="text-white font-bold w-full text-9xl">TOURNOIS ARAM</h2>
         <div className="flex gap-4 w-full justify-center">
           <button className="bg-primary text-secondary px-12 py-3 text-2xl mt-6 rounded-sm">Inscription</button>
           <button className="bg-none border text-white px-12 py-3 text-2xl mt-6 rounded-sm">Contacter</button>
