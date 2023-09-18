024fedb6c1c83b0d0435521bfdc7d3d81383fe15
John Barry <john.olivierb@gmail.com>
2023-09-18 15:42:28 -0400

    Added opengraph-image

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,13 +14,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <title>Tournois HAQ</title>
-        <meta name="description" content="HAQ - Tournois ARAM League of Legend" />
-        <meta property="og:title" content="Tournois HAQ" />
-        <meta property="og:description" content="HAQ - Tournois ARAM League of Legend" />
-        <meta property="og:image" content="/public/banner.png" />
-      </Head>
       <body>{children}</body>
     </html>
   )
