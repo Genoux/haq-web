4b91194172970d9274be7915d054e21d4e2cea8b
John Barry <john.olivierb@gmail.com>
2024-01-15 14:16:16 -0500

    Use vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "@/app/components/ui/theme-provider";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: 'Tournois HAQ',
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body className={`${GeistSans.className} bg-black`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
